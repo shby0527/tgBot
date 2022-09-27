6286ff85ba77d8cade1e98941a498f74299711cf
Hayama Umi <umi@umiblog.cn>
2022-09-27 22:33:55 +0800

    QvQ

--- src/main/java/com/github/shby0527/tgbot/TelegramBotApplication.java
+++ src/main/java/com/github/shby0527/tgbot/TelegramBotApplication.java
@@ -4,12 +4,14 @@ package com.github.shby0527.tgbot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync
 @EnableScheduling
 @SpringBootApplication
+@EnableCaching
 @MapperScan(basePackages = {"com.github.shby0527.tgbot.dao"})
 public class TelegramBotApplication {
 
